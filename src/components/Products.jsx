3853f5f54c38dd3272fe80634b7a75293523021e
Rafia Shehnil <shehnilrafia@gmail.com>
2024-06-30 21:06:55 +0600

    Commit changes

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,118 @@
-import React from "react";
+import React from 'react'
 
-const Products = () => {
-  return <div>
-    <h1>Products</h1>
-    </div>;
-};
+const products = [
+  {
+    id: 1,
+    name: 'Red Roses',
+    href: '#',
+    imageSrc: 'https://images.pexels.com/photos/4825/red-love-romantic-flowers.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    imageAlt: "Rose.",
+    price: 'Tk 50',
+    //color: 'Black',
+  },
+  {
+    id: 2,
+    name: 'Gladiolus',
+    href: '#',
+    imageSrc: 'https://images.pexels.com/photos/6145089/pexels-photo-6145089.jpeg?auto=compress&cs=tinysrgb&w=600',
+    imageAlt: "Gladiolus.",
+    price: 'Tk 70',
+    //color: 'Black',
+  },
+  {
+    id: 3,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://images.pexels.com/photos/2360538/pexels-photo-2360538.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1g',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+]
 
-export default Products;
+export default function Example() {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Welcome to Rafia's Flower Shop!</h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <div key={product.id} className="group relative">
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  src={product.imageSrc}
+                  alt={product.imageAlt}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <a href={product.href}>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.name}
+                    </a>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">{product.price}</p>
+              </div>
+              <button
+                    onClick={() => addToCart(product.id)}
+                    className="ml-2 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                  >
+                    Add to Cart
+                  </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
